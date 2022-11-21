9a19574bf678232f5da28b00c5ebe8af6fb3fe87
ghew <galenhew@gmail.com>
2022-11-21 19:15:39 +0800

    streamlit updates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from dashboard import FinTweepy
 from streamlit_autorefresh import st_autorefresh
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+
 
 # tweet = FinTweepy(config)
 # df_author = tweet.get_author_df()
@@ -8,3 +12,9 @@ from streamlit_autorefresh import st_autorefresh
 # df_all_users_timeline = tweet.get_all_users_timeline()
 
 st.title("Stocks")
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["google-bigquery"]
+)
+client = bigquery.Client(credentials=credentials)
