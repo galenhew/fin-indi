5f369f44ee08efcf6a81727bfa9b012b39cf61f1
ghew <galenhew@gmail.com>
2022-11-21 22:01:32 +0800

    streamlit updates

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ from google.cloud import bigquery
 
 st.title("Stocks")
 
+config= st.secrets
+tweet = FinTweepy(config)
+df_author = tweet.get_author_df()
+df_timeline = tweet.get_user_timeline('from:jimcramer')
+df_all_users_timeline = tweet.get_all_users_timeline()
+
+st.dataframe(df_all_users_timeline)
+
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["google-bigquery"]
 )
 client = bigquery.Client(credentials=credentials)
+
